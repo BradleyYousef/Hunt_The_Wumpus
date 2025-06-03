f4f2aede0443d731c3eb412cfe59fcf73de4540d
BradleyYousef <bradley.yousef@education.nsw.gov.au>
2025-06-03 02:11:59 +0000

    Added code to move between different cave objects

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from cave import Cave
 cavern = Cave("Cavern")
 cavern.set_description("A damp and dirty cave")
 grotto = Cave("Grotto")
-grotto.set_description = Cave("A small cave with ancient markings")
+grotto.set_description("A small cave with ancient markings")
 dungeon = Cave("Dungeon")
 dungeon.set_description("A large cave with a rack")
 
@@ -12,6 +12,10 @@ dungeon.link_caves(cavern, "North")
 dungeon.link_caves(grotto, "West")
 grotto.link_caves(dungeon, "East")
 
-cavern.get_details()
-dungeon.get_details()
-grotto.get_details()
+current_cave = cavern
+while True:
+    print("\n")
+    current_cave.get_details()
+    command = input("> ")
+    current_cave = current_cave.move(command)
+    
