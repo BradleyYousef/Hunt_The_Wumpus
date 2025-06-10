190cf9d26990d4706636b49f4abb779c9580e11e
BradleyYousef <bradley.yousef@education.nsw.gov.au>
2025-06-10 04:54:25 +0000

    Began working on the new cave system

--- main.py
+++ main.py
@@ -8,6 +8,41 @@ grotto = Cave("Grotto")
 grotto.set_description("A small cave with ancient markings")
 dungeon = Cave("Dungeon")
 dungeon.set_description("A large cave with a rack")
+mineshaft = Cave("Mineshaft Entrance")
+
+fork = Cave("Fork In The Road")
+fork.set_description("A fork in the caves pathway")
+grand = Cave("Grand Marsh")
+grand.set_description("A swampy wetland covered in moss")
+library = Cave("Sunken Library")
+library.set_description("A withered library trapped in the moss")
+thicket = Cave("Deadly Thicket")
+thicket.set_description("A forest with many withered trees")
+dump = Cave("Wet Dump")
+dump.set_description("A wet pile of moss that appears to be smoking")
+shopkeeper = Cave("Shopkeeper's Abode")
+shopkeeper.set_description("A cabin hidden in the moss where the shopkeeper resides")
+hole = Cave("Giant Hole")
+hole.set_description("A giant hole that seems hard to get out of")
+stairway = Cave("Royal Stairway")
+stairway.set_description("A royal stairway with many armour stands lining the path")
+citadel = Cave("Citadel")
+citadel.set_description("The former sanctuary for civilisation")
+path = Cave("Ruined Pathway")
+path.set_description("")
+shrine = Cave("Ancient Shrine")
+shrine
+insides = Cave("Ancient Shrine Insides")
+insides
+highland = Cave("Highland Glades")
+highland
+city = Cave("Sky City")
+city
+temple = Cave("Sky Temple")
+temple
+arena = Cave("Sky Arena")
+arena
+
 
 harry = Enemy("Harry", "A dirty, smelly Wumpus")
 harry.set_conversation("Come closer. I cannot see you.")
@@ -15,9 +50,7 @@ harry.set_weakness("vegemite")
 dungeon.set_character(harry)
 
 cavern.link_caves(dungeon, "South")
-dungeon.link_caves(cavern, "North")
-dungeon.link_caves(grotto, "West")
-grotto.link_caves(dungeon, "East")
+
 
 current_cave = cavern
 dead = False
