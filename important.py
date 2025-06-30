5cb51c30bd3b19901f502494a0d7a004900b7139
BradleyYousef <bradley.yousef@education.nsw.gov.au>
2025-06-30 01:00:40 +0000

    Began working on the inventory class again

---
+++ important.py
@@ -0,0 +1,14 @@
+var = 99
+print(var)
+if var == 99:
+    var = 100
+    print(var)
+    if var == 100:
+        var = 101
+        print(var)
+        if var == 101:
+            var = 102
+            print(var)
+            if var == 102:
+                var = 103
+                print(var)
---
+++ inventory.py
@@ -0,0 +1,7 @@
+class Inventory:
+    def __init__(self):
+        self.name = None
+        self.quantity = 0
+        self.price = 0
+        #def __str__(self):
+         #   return f"{self.name} (Quantity: {self.quantity}, Price: ${self.price:.2f})"
